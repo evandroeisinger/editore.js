8e44cd85d6f2d7915695ab007ffbf6dee828cd61
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-21 20:37:22 -0200

    add applyEditable and applyTabIndex methods to use on editor constructor

--- src/editor.js
+++ src/editor.js
@@ -10,14 +10,16 @@
 
   function Editor(form) {
     var self = this;
-    
-    self.form   = form;
-    self.fields = [];
 
     if (!form || !form.nodeName)
       return new Error('No form was passed!');
 
+    self.form   = form;
     self.fields = self.getFields(self.form);
+
+    if (self.fields.length)
+      for (var field in self.fields)
+        self.applyFieldEvents(self.fields[field]);
 
     return {
       fields: self.fields
@@ -29,14 +31,20 @@
       var self    = this,
           fields  = [];
 
-      for (var i = form.children.length - 1; i >= 0; i--)(function(element) {
-        var field = self.getDataAttribute('field', element);
+      if (!form.children.length)
+        return fields;
+
+      for (var i = form.children.length - 1; i >= 0; i--) (function(element) {
+        var field = self.getDataAttribute('field', element),
+            tabIndex = (i - length);
 
         if (field)
           fields.push({
-            name: field,
-            length: self.getDataAttribute('length', element, true),
-            placeholder: self.getDataAttribute('placeholder', element)
+            name        : field,
+            typeof      : self.getDataAttribute('type', element),
+            length      : self.getDataAttribute('length', element, true),
+            placeholder : self.getDataAttribute('placeholder', element),
+            element     : self.applyEditable(self.applyTabIndex(element, tabIndex))
           });
       }(form.children[i]));
 
@@ -50,7 +58,24 @@
         return window.parseInt(value);
       else
         return value;
-    }
+    },
+
+    applyEditable: function(element) {
+      element.setAttribute('contenteditable', true);
+      return element; 
+    },
+
+    applyTabIndex: function(element, index) {
+      element.setAttribute('tabindex', index + 1);
+      return element; 
+    },
+
+    applyFieldEvents: function(field) {
+      // placeholder
+      // length
+    },
+
+    applyType: function(field, type) {}
   }
 
   return Editor;
