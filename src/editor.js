e7f8ba9bf473f8e0337753d8fef3d18c6d17033e
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-21 16:37:48 -0200

    create library structure

--- src/editor.js
+++ src/editor.js
@@ -1,9 +1,26 @@
-!function(global, document) {
+(function(global, editor) {
+  if (typeof define === 'function' && define.amd)
+    define('editor-js', [], editor);
+  else if (typeof exports !== 'undefined')
+    exports.Editor = editor();
+  else
+    global.Editor = editor();
+}(window, function() {
   'use strict';
 
-  global.Editor = function(form) {
-    var self = this;
+  function Editor(form) {
+    var self   = this,
+        fields = [];
+
+    if (!form || !form.nodeName)
+      return new Error('No form was passed!');
+
+    return {
+      fields: fields
+    }
   }
 
-  Editor.prototype = {};
-}(window, document);
+  Editor.prototype = {}
+
+  return Editor;
+}));
