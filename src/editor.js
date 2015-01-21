8ad843a10c37f980146cc776b07e2979a2548234
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-21 18:12:04 -0200

    add getDataAttributes and getFields prototype methods

--- src/editor.js
+++ src/editor.js
@@ -9,18 +9,49 @@
   'use strict';
 
   function Editor(form) {
-    var self   = this,
-        fields = [];
+    var self = this;
+    
+    self.form   = form;
+    self.fields = [];
 
     if (!form || !form.nodeName)
       return new Error('No form was passed!');
 
+    self.fields = self.getFields(self.form);
+
     return {
-      fields: fields
+      fields: self.fields
     }
   }
 
-  Editor.prototype = {}
+  Editor.prototype = {
+    getFields: function(form) {
+      var self    = this,
+          fields  = [];
+
+      for (var i = form.children.length - 1; i >= 0; i--)(function(element) {
+        var field = self.getDataAttribute('field', element);
+
+        if (field)
+          fields.push({
+            name: field,
+            length: self.getDataAttribute('length', element, true),
+            placeholder: self.getDataAttribute('placeholder', element)
+          });
+      }(form.children[i]));
+
+      return fields;
+    },
+
+    getDataAttribute: function(name, element, isInt) {
+      var value = element.getAttribute('data-' + name) || false;
+
+      if (isInt)
+        return window.parseInt(value);
+      else
+        return value;
+    }
+  }
 
   return Editor;
 }));
