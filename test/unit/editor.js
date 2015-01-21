417a01a82640fa6aeb69f12fc461cfe6676969f3
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-21 16:39:11 -0200

    test some initial features

--- test/unit/editor.js
+++ test/unit/editor.js
@@ -1,5 +1,11 @@
-describe("editor.js - unit", function() {
-  it("dummy", function() {
-    expect(true).toBeDefined(true);
+describe('editor.js - unit', function() {
+  it('return a Editor instance', function() {
+    var form = document.createElement('form');
+    expect((new Editor(form)).fields).toBeDefined(true);
+  });
+
+  it('return a Error no form was passed', function() {
+    expect(new Editor('not a html element') instanceof Error).toBe(true);
+    expect(new Editor() instanceof Error).toBe(true);
   });
 });
