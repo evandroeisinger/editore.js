dc37e2243ad5f6f60b396bbada39604509dcf243
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-23 16:55:26 -0200

    add getValues and getFields methods

--- src/editor.js
+++ src/editor.js
@@ -30,8 +30,8 @@
     self.regex.space  = /\s/g;
     self.regex.spaceAndEnbsp  = /\s|&nbsp;/g;
 
-    // handler methods
-    function handler(methods, data, context) {
+    // set handler event method
+    function setHandler(methods, data, context) {
       return function(e) {
         for (var method in methods) (function (method) {
           method.call(context, data, e);
@@ -39,69 +39,111 @@
       }
     }
 
-    // editor init
-    if (self.fields.length)
-      for (var field in self.fields) (function (field) {
-        var click   = [],
-            focus   = [],
-            keyup   = [self.calculateLength],
-            keydown = [];
+    // return editor fields 
+    function getFields() {
+      var fields = {};
 
-        if (field.maxLength) {
-          keyup.push(self.validateLength);
+      for (var field in self.fields) (function(field) {
+        fields[field.name] = {
+          name        : field.name,
+          element     : field.element,
+          value       : field.value(field),
+          maxLength   : field.maxLength,
+          type        : field.type,
+          require     : field.require,
+          placeholder : field.placeholder
         }
-
-        if (field.placeholder) {
-          keyup.push(self.validatePlaceholder);
-          keydown.push(self.removePlaceHolder);
-        }
-
-        if (field.require) {
-          keyup.push(self.validateRequire);
-        }
-
-        field.element.addEventListener('click', handler(click, field, self));
-        field.element.addEventListener('focus', handler(focus, field, self));
-        field.element.addEventListener('keyup', handler(keyup, field, self));
-        field.element.addEventListener('keydown', handler(keydown, field, self));
-
-        self
-          .calculateLength(field)
-          .validateLength(field)
-          .validatePlaceholder(field)
-          .validateRequire(field);
-
       }(self.fields[field]))
 
+      return fields;
+    }
+
+    // return field values
+    function getValues() {
+      var values = {};
+
+      for (var field in self.fields) (function(field) {
+        values[field.name] = {
+          name   : field.name,
+          value  : field.value(field),
+          length : field.length,
+          valid  : field.valid
+        }
+      }(self.fields[field]))
+
+      return values;
+    }
+
+    // editor constructor
+    for (var field in self.fields) (function (field) {
+      var click   = [],
+          focus   = [],
+          keyup   = [self.calculateLength],
+          keydown = [];
+
+      if (field.maxLength) {
+        keyup.push(self.validateLength);
+      }
+
+      if (field.placeholder) {
+        keyup.push(self.validatePlaceholder);
+        keydown.push(self.removePlaceHolder);
+      }
+
+      if (field.require) {
+        keyup.push(self.validateRequire);
+      }
+
+      field.element.addEventListener('click', setHandler(click, field, self));
+      field.element.addEventListener('focus', setHandler(focus, field, self));
+      field.element.addEventListener('keyup', setHandler(keyup, field, self));
+      field.element.addEventListener('keydown', setHandler(keydown, field, self));
+
+      self
+        .calculateLength(field)
+        .validateLength(field)
+        .validatePlaceholder(field)
+        .validateRequire(field);
+
+    }(self.fields[field]))
+
     return {
-      fields: self.fields
+      fields : getFields,
+      values : getValues
     }
   }
 
   Editor.prototype = {
     getFields: function(form) {
       var self    = this,
-          fields  = [];
+          fields  = {};
 
       if (!form.children.length)
         return fields;
 
       for (var i = form.children.length - 1; i >= 0; i--) (function(element) {
-        var field = self.getDataAttribute('field', element),
+        var field    = self.getDataAttribute('field', element),
             tabIndex = (i - length);
 
         if (field)
-          fields.push({
+          fields[field] = {
             name        : field,
             type        : self.getDataAttribute('type', element) || 'simple',
             maxLength   : self.getDataAttribute('length', element, true),
             placeholder : self.getDataAttribute('placeholder', element),
             require     : self.getDataAttribute('require', element),
-            element     : self.applyEditable(self.applyTabIndex(element, tabIndex))
-          });
+            element     : self.applyEditable(self.applyTabIndex(element, tabIndex)),
+            value       : self.getValue,
+            valid       : false
+          }
       }(form.children[i]));
 
       return fields;
+    },
+
+    getValue: function(field) {
+      var self = this;
+      return '';
     },
 
     getDataAttribute: function(name, element, isInt) {
