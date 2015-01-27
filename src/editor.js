2f5d4bea31a17b4373f0463bb81bd51abec49c79
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-27 17:27:21 -0200

    create getValue method

--- src/editor.js
+++ src/editor.js
@@ -14,20 +14,22 @@
     if (!form || !form.nodeName)
       return new Error('No form was passed!');
 
-    // elements
-    self.form   = form;
-    self.fields = self.getFields(self.form);
-    
     // field types
     self.types        = {};
     self.types.RICH   = 'rich';
     self.types.SIMPLE = 'simple';
 
+    // elements
+    self.form   = form;
+    self.fields = self.getFields(self.form);
+    
     // regex patterns
-    self.regex        = {};
-    self.regex.markup = /(<\/*[\w\s01-9='":;,\-]*\/*>)+/g;
-    self.regex.enbsp  = /&nbsp;*/g;
-    self.regex.space  = /\s/g;
+    self.regex            = {};
+    self.regex.markup     = /(<\/*[\w\s01-9='":;,\-]*\/*>)+/g;
+    self.regex.enbsp      = /&nbsp;*/g;
+    self.regex.space      = /\s/g;
+    self.regex.trim       = /\s+$/g;
+    self.regex.lineBreak  = /(\r\n|\n|\r)[.]?/g; 
     self.regex.spaceAndEnbsp  = /\s|&nbsp;/g;
 
     // set handler event method
@@ -39,7 +41,7 @@
       }
     }
 
-    // return editor fields NAME REVIEW!!!!
+    // return editor fields
     function fields() {
       var fields = {};
 
@@ -47,7 +49,6 @@
         fields[field.name] = {
           name        : field.name,
           element     : field.element,
-          value       : field.value(field),
           maxLength   : field.maxLength,
           type        : field.type,
           require     : field.require,
@@ -65,42 +66,33 @@
       for (var field in self.fields) (function(field) {
         values[field.name] = {
           name   : field.name,
-          value  : field.value(field),
-          length : field.length,
-          valid  : field.valid(field)
+          value  : self.getValue(field),
+          length : self.getLength(field),
+          valid  : self.isValid(field)
         }
       }(self.fields[field]))
 
       return values;
     }
 
-    function validate() {
-      for (var field in self.fields) (function (field) {
-        self
-        .calculateLength(field)
-        .validateLength(field)
-        .validateRequire(field);
-      }(self.fields[field]))
-    }
-
     // editor constructor
     for (var field in self.fields) (function (field) {
       var click   = [],
           focus   = [],
-          keyup   = [self.calculateLength],
+          keyup   = [],
           keydown = [];
 
       if (field.maxLength) {
-        keyup.push(self.validateLength);
+        keyup.push(self.isOutOfBounds);
       }
 
       if (field.placeholder) {
-        keyup.push(self.validatePlaceholder);
+        keyup.push(self.applyPlaceholder);
         keydown.push(self.removePlaceHolder);
       }
 
       if (field.require) {
-        keyup.push(self.validateRequire);
+        keyup.push(self.isEmpty);
       }
 
       field.element.addEventListener('click', handler(click, field, self));
@@ -108,16 +100,12 @@
       field.element.addEventListener('keyup', handler(keyup, field, self));
       field.element.addEventListener('keydown', handler(keydown, field, self));
 
-      self
-        .calculateLength(field)
-        .validatePlaceholder(field);
-
-    }(self.fields[field]))
+      self.applyPlaceholder(field);
+    } (self.fields[field]));
 
     return {
-      fields   : fields,
-      values   : values,
-      validate : validate
+      fields : fields,
+      values : values,
     }
   }
 
@@ -136,13 +124,13 @@
         if (field) {
           fields[field] = {
           name        : field,
-          type        : self.getDataAttribute('type', element, 'str', 'single'),
+          type        : self.getDataAttribute('type', element, 'str', self.types.SIMPLE),
           maxLength   : self.getDataAttribute('length', element, 'int', false),
           placeholder : self.getDataAttribute('placeholder', element, 'str', false),
           require     : self.getDataAttribute('require', element, 'bol', false),
           element     : self.applyEditable(self.applyTabIndex(element, tabIndex)),
-          value       : self.getValue,
-          valid       : self.getValid,
+          value       : '',
+          valid       : false,
           length      : 0
           }
         }
@@ -153,15 +141,12 @@
 
     getValue: function(field) {
       var self = this;
-      return '';
-    },
 
-    getValid: function(field) {
-      if (field.require && field.element.classList.contains('require'))
-        return false;
-      if (field.maxLength && field.element.classList.contains('invalid'))
-        return false;
-      return true;
+      if (field.type = self.types.SIMPLE)
+        return field.element.innerText.replace(self.regex.lineBreak, ' ').replace(self.regex.trim, '');
+      if (field.type = self.types.RICH)
+        return field.element.innerHTML;
+      return '';
     },
 
     getDataAttribute: function(name, element, type, defaultValue) {
@@ -188,27 +173,13 @@
       return value;
     },
 
-    getInnerText: function(element) {
-      var self = this;
-      return element.innerHTML
-        .replace(self.regex.markup, '')
-        .replace(self.regex.enbsp, '');
-    },
-
-    getClassName: function(element, className) {
-      if (element.classList)
-        return element.classList.contains(className);
-    },
-
-    calculateLength: function(field) {
+    getLength: function(field) {
       var self = this;
 
-      field.length = field.element.innerHTML
+      return field.element.innerHTML
         .replace(self.regex.markup, '')
         .replace(self.regex.spaceAndEnbsp, '_')
         .length;
-
-      return self;
     },
 
     applyEditable: function(element) {
@@ -221,6 +192,14 @@
       return element; 
     },
 
+    applyPlaceholder: function(field) {
+      var self = this;
+
+      if (!self.getLength(field))
+        field.element.classList.add('placeholder');
+      return self;
+    },
+
     removePlaceHolder: function(field, event) {
       var self = this;
       
@@ -228,32 +207,38 @@
         field.element.classList.remove('placeholder');
     },
 
-    validateLength: function(field) {
+    isOutOfBounds: function(field) {
       var self = this;
 
-      if (field.length > field.maxLength) 
+      if (self.getLength(field) > field.maxLength) {
         field.element.classList.add('invalid');
-      else
-        field.element.classList.remove('invalid')
-      return self;
+        return true;
+      }
+      
+      field.element.classList.remove('invalid')
+      return false;
     },
 
-    validateRequire: function(field) {
+    isEmpty: function(field) {
       var self = this;
 
-      if (!field.length) 
+      if (!self.getLength(field)) {
         field.element.classList.add('require');
-      else
-        field.element.classList.remove('require')
-      return self;
+        return true;
+      }
+
+      field.element.classList.remove('require');
+      return false;
     },
 
-    validatePlaceholder: function(field) {
+    isValid: function(field) {
       var self = this;
 
-      if (!field.length)
-        field.element.classList.add('placeholder');
-      return self;
+      if (field.require && self.isEmpty(field))
+        return false;
+      if (field.maxLength && self.isOutOfBounds(field))
+        return false;
+      return true;
     }
   }
 
