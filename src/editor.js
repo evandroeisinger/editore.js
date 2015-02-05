e7261d622d8bef09bc08e867c9831be44e77c0b5
Evandro Eisinger <evandroeisinger@gmail.com>
2015-02-05 18:11:00 -0200

    improve components and plugins constructors

--- src/editor.js
+++ src/editor.js
@@ -22,6 +22,29 @@
     self.eventTypes = {};
     self.eventTypes.INPUT = [];
 
+    // editor components
+    self.components = {
+      insert: {
+        element: document.createElement('div'),
+        plugins: [],
+        status: false
+      },
+      edition: {
+        element: document.createElement('div'),
+        plugins: [],
+        status: false
+      }
+    };
+
+    // set action element      
+    self.components.insert.element.setAttribute('contenteditable', 'false');
+    self.components.insert.element.setAttribute('id', 'insertComponent');
+    // set edition element      
+    self.components.edition.element.setAttribute('contenteditable', 'false');
+    self.components.edition.element.setAttribute('id', 'editionComponent');
+    self.components.edition.element.style.position = 'absolute';
+    self.components.edition.element.style.zIndex = 9999;
+
     // field types
     self.fieldTypes         = {};
     self.fieldTypes.RICH    = 'rich';
@@ -38,18 +61,9 @@
     self.regex.lineBreaks     = /(\r\n|\n|\r)[.]?/g; 
     self.regex.spaceAndEnbsp  = /\s|&nbsp;/g;
 
-    // set editor form, fields and plugins
+    // set editor form and fields
     self.form             = form;
     self.fields           = {};
-
-    // set handler event method
-    function handler(methods, data, context) {
-      return function(e) {
-        for (var method in methods) (function (method) {
-          method.call(context, data, e);
-        }(methods[method]));
-      }
-    }
 
     // return editor fields
     function fields() {
@@ -62,8 +76,7 @@
           maxLength   : field.maxLength,
           type        : field.type,
           required     : field.required,
-          placeholder : field.placeholder,
-          plugins     : field.plugins,
+          placeholder : field.placeholder
         }
       }(self.fields[field]))
 
@@ -87,35 +100,37 @@
     }
 
     // register plugins
-    function register(type, Plugin) {
-      for (var field in self.fields) (function(field) {
-        if (field.type == self.fieldTypes.RICH) {
-          var plugin = new Plugin(field.plugins[type], field, self);
-          field.plugins[type].methods[plugin.name] = plugin;
-          field.plugins[type].element.appendChild(plugin.register());
-        }
-      } (self.fields[field]));
+    function register(component, Plugin) {
+      if (!self.components[component] || !Plugin)
+        return new Error('invalid component type or plugin');
+
+      var plugin = new Plugin(self);
+      self.components[component].plugins[plugin.name] = plugin;
+      self.components[component].element.appendChild(plugin.button);
     }
 
     // destroy editor listeners
     function destroy() {
+      // unset fields
       for (var field in self.fields) (function(field) {
-        // set handlers
+        field.element.removeAttribute('contenteditable');
         field.element.removeEventListener('paste', field.events.paste);
         field.element.removeEventListener('click', field.events.click);
         field.element.removeEventListener('mouseup', field.events.mouseup);
         field.element.removeEventListener('keydown', field.events.keydown);
         field.element.removeEventListener('keypress', field.events.keypress);
         field.element.removeEventListener('keyup', field.events.keyup);
-
-        if (field.type == self.fieldTypes.RICH) {
-          for (var plugin in field.plugins) (function(plugin) {
-            for (var method in plugin.methods) (function(method) {
-              method.destroy();
-            } (plugin.methods[method]));
-          } (field.plugins[plugin]));
-        }
       } (self.fields[field]));
+      // unset components
+      for (var component in self.components) (function(component) {
+        for (var plugin in component.plugins) (function(plugin) {
+          plugin.beforeDestroy(component);
+          if (plugin._action)
+            plugin.button.removeEventListener('click', plugin._action);
+        } (component.plugins[plugin]));
+        if (component.status)
+          component.element.parentNode.removeChild(component.element);
+      } (self.components[component]));
     }
 
     // register callbacks to editor events
@@ -153,28 +168,6 @@
       self.fields[field].length      = 0;
       self.fields[field].focus       = false;
       self.fields[field].events      = {};
-      self.fields[field].plugins     = {
-        action: {
-          element: document.createElement('div'),
-          methods: {}
-        },
-
-        edition: {
-          element: document.createElement('div'),
-          methods: {},
-          status: false
-        }
-      };
-
-      // set action element      
-      self.fields[field].plugins.action.element.setAttribute('contenteditable', 'false');
-      self.fields[field].plugins.action.element.setAttribute('id', 'actionBar');
-      // set edition element      
-      self.fields[field].plugins.edition.element.setAttribute('contenteditable', 'false');
-      self.fields[field].plugins.edition.element.setAttribute('id', 'editionBar');
-      self.fields[field].plugins.edition.element.style.position = 'absolute';
-      self.fields[field].plugins.edition.element.style.zIndex = 9999;
-
       // set field listeners
       switch(self.fields[field].type) {
         case self.fieldTypes.SIMPLE:
@@ -200,19 +193,22 @@
       if (self.fields[field].required)
         keyupEvents.push(self.validateRequire);
 
-      // set elements
-      self.setEditable(self.fields[field].element);
-      self.setTabIndex(self.fields[field].element, (i - length) + 1);
+      // set field element
+      self.fields[field].element.style.position = 'relative';
+      self.fields[field].element.style.minHeight = '1em'; //fix empty contenteditable input
+      self.fields[field].element.setAttribute('contenteditable', true);
+      self.fields[field].element.setAttribute('tabindex', (i - length) + 1);
       self.setLength(self.fields[field]);
       self.setPlaceholder(self.fields[field]);
+
       // set handlers    
-      self.fields[field].events.paste = handler(pasteEvents, self.fields[field], self);
-      self.fields[field].events.click = handler(clickEvents, self.fields[field], self);
-      self.fields[field].events.mouseup = handler(mouseUpEvents, self.fields[field], self);
-      self.fields[field].events.keydown = handler(keydownEvents, self.fields[field], self);
-      self.fields[field].events.keypress = handler(keypressEvents, self.fields[field], self);
-      self.fields[field].events.keyup = handler(keyupEvents, self.fields[field], self);
-      self.fields[field].events.DOMNodeInserted = handler(DOMNodeInsertedEvents, self.fields[field], self)
+      self.fields[field].events.paste = self.handler(pasteEvents, self.fields[field], self);
+      self.fields[field].events.click = self.handler(clickEvents, self.fields[field], self);
+      self.fields[field].events.mouseup = self.handler(mouseUpEvents, self.fields[field], self);
+      self.fields[field].events.keydown = self.handler(keydownEvents, self.fields[field], self);
+      self.fields[field].events.keypress = self.handler(keypressEvents, self.fields[field], self);
+      self.fields[field].events.keyup = self.handler(keyupEvents, self.fields[field], self);
+      self.fields[field].events.DOMNodeInserted = self.handler(DOMNodeInsertedEvents, self.fields[field], self)
       // atach handlers
       self.fields[field].element.addEventListener('paste', self.fields[field].events.paste);
       self.fields[field].element.addEventListener('click', self.fields[field].events.click);
@@ -242,41 +238,41 @@
             top,
             left;
 
-        if (selection.type == 'Range' && !field.plugins.edition.status) {
-          for (var method in field.plugins.edition.methods) (function(method) {
-            method.beforeShow();
-          } (field.plugins.edition.methods[method]));
-          // show edition toolbar
-          document.body.appendChild(field.plugins.edition.element);
+        if (selection.type == 'Range' && !self.components.edition.status) {
+          self.setComponent('edition', field);
           range = selection.getRangeAt(0);
           position = range.getBoundingClientRect();
-          top = position.top + window.pageYOffset - field.plugins.edition.element.offsetHeight;
-          left = ((position.left + position.right) / 2) - (field.plugins.edition.element.offsetWidth / 2);
-          field.plugins.edition.element.style.top =  top + 'px';
-          field.plugins.edition.element.style.left = left + 'px';
-          field.plugins.edition.status = true;
-          field.plugins.edition.selection = selection;
+          top = position.top + window.pageYOffset - self.components.edition.element.offsetHeight;
+          left = ((position.left + position.right) / 2) - (self.components.edition.element.offsetWidth / 2);
+          self.components.edition.element.style.top =  top + 'px';
+          self.components.edition.element.style.left = left + 'px';
+          self.components.edition.status = true;
+          self.components.edition.selection = selection;
           return;
         }
 
-        if(field.plugins.edition.status) {
-          document.body.removeChild(field.plugins.edition.element);
-          field.plugins.edition.status = false;
-          field.plugins.edition.selection = false;
+        if(self.components.edition.status) {
+          document.body.removeChild(self.components.edition.element);
+          self.components.edition.status = false;
+          self.components.edition.selection = false;
         }
       },
 
       focus: function(field, e) {
-        var self = this;
+        var self = this,
+            currentBlock;
 
-        if ([91,40,38,37,39,13,1, 8].indexOf(e.which) < 0 || (!field.length & e.type !== 'click') || e.target == field.plugins.action.element || e.target == field.plugins.edition.element)
+        if ([91,40,38,37,39,13,1, 8].indexOf(e.which) < 0 || (!field.length & e.type !== 'click') || e.target == self.components.insert.element || e.target == self.components.edition.element)
           return;
-
+        
         field.focus = true;
         field.element.classList.add('focus');
+        currentBlock = self.getCurrentBlock(self.getCurrentNode());
 
-        if (field.type == self.fieldTypes.RICH)
-          self.setAction(field);
+        if (field.type == self.fieldTypes.RICH && field.currentBlock !== currentBlock) {
+          field.currentBlock = currentBlock;
+          self.setComponent('insert', field);
+        }
         
         for (var _field in self.fields) (function (_field) {
           if (_field.name == field.name)
@@ -284,9 +280,6 @@
           // remove focus
           _field.focus = false;
           _field.element.classList.remove('focus');
-          // remove actionBar
-          if (_field.type == self.fieldTypes.RICH && _field.currentBlock)
-            self.unsetAction(_field);
         } (self.fields[_field]));
       },
 
@@ -403,18 +396,30 @@
       return self;
     },
 
-    setAction: function(field) {
-      var self = this,
-          currentBlock = self.getCurrentBlock(self.getCurrentNode());
+    setComponent: function(component, field) {
+      var self = this;
+          
+      for (var plugin in self.components[component].plugins) (function(plugin) {
+        if (plugin._action)
+          plugin.button.removeEventListener('click', plugin._action);
+        plugin._action = self.handler([plugin.action], field, plugin);
+        plugin.button.addEventListener('click', plugin._action);
+        plugin.beforeShow(self.components[component], field);
+      } (self.components[component].plugins[plugin]));
 
-      if (field.currentBlock !== currentBlock) {
-        field.currentBlock = currentBlock;
-         for (var method in field.plugins.action.methods) (function(method) {
-              method.beforeShow();
-        } (field.plugins.action.methods[method]));
-        // set action toolbar
-        field.element.insertBefore(field.plugins.action.element, field.currentBlock.nextSibling);
+      switch(component) {
+        case 'insert':
+          // set insert component
+          field.element.insertBefore(self.components.insert.element, field.currentBlock.nextSibling);
+          self.components.insert.status = true;
+          break;
+        case 'edition':
+          // set edition component
+          document.body.appendChild(self.components.edition.element);
+          self.components.edition.status = true;
+          break;
       }
+
       return self;
     },
 
@@ -430,21 +435,6 @@
       return self;
     },
 
-    setEditable: function(element) {
-      var self = this;
-
-      element.style.minHeight = '1em'; //fix empty contenteditable input
-      element.setAttribute('contenteditable', true);
-      return self; 
-    },
-
-    setTabIndex: function(element, index) {
-      var self = this;
-
-      element.setAttribute('tabindex', index);
-      return self; 
-    },
-
     // https://code.google.com/p/chromium/issues/detail?id=226941
     unsetSpan: function(field, event) {
       var self = this,
@@ -455,15 +445,6 @@
 
       span.parentNode.insertBefore(document.createTextNode(span.innerText), span);
       span.parentNode.removeChild(span);
-      return self;
-    },
-
-    unsetAction: function(field) {
-      var self = this;
-      
-      if (field.element.children.length)
-        field.element.removeChild(field.plugins.action.element);
-      field.currentBlock = null;
       return self;
     },
 
@@ -506,6 +487,14 @@
 
       field.element.classList.remove('required');
       return false;
+    },
+
+    handler: function(methods, data, context) {
+      return function(e) {
+        for (var method in methods) (function (method) {
+          method.call(context, data, e);
+        }(methods[method]));
+      }
     },
 
     emmit: function(event, data) {
