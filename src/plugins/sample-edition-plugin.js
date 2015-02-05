28ab2559945cc067d95e2a9e28c169bd0cece3a7
Evandro Eisinger <evandroeisinger@gmail.com>
2015-02-05 18:11:45 -0200

    change plugins namespaces

---
+++ src/plugins/sample-edition-plugin.js
@@ -0,0 +1,35 @@
+(function(global, plugin) {
+  if (typeof define === 'function' && define.amd)
+    define('sample-edition-plugin', ['editor-js'], plugin);
+  else if (typeof exports !== 'undefined')
+    exports.SamplEditionPlugin = plugin();
+  else
+    global.SampleEditionPlugin = plugin();
+}(window, function() {
+  'use strict';
+
+  function SampleEditionPlugin(editor) {
+    this.name = 'SampleEditionPlugin';
+    this.editor = editor;
+    this.button = document.createElement('button');
+    this.button.innerText = 'edition-plugin';
+  }
+
+  SampleEditionPlugin.prototype = {
+    action: function(field, e) {
+      e.preventDefault();
+      console.log('edition',field);
+    },
+
+    beforeShow: function(component, field) {
+      return;
+    },
+
+    beforeDestroy: function(component) {
+      return;
+    }
+  }
+
+  return SampleEditionPlugin;
+}));
+    
---
+++ src/plugins/sample-insert-plugin.js
@@ -0,0 +1,35 @@
+(function(global, plugin) {
+  if (typeof define === 'function' && define.amd)
+    define('sample-insert-plugin', ['editor-js'], plugin);
+  else if (typeof exports !== 'undefined')
+    exports.SampleInsertPlugin = plugin();
+  else
+    global.SampleInsertPlugin = plugin();
+}(window, function() {
+  'use strict';
+
+  function SampleInsertPlugin(editor) {
+    this.name = 'SampleInsertPlugin';
+    this.editor = editor;
+    this.button = document.createElement('button');
+    this.button.innerText = 'edition-plugin';
+  }
+
+  SampleInsertPlugin.prototype = {
+    action: function(field, e) {
+      e.preventDefault();
+      console.log('insert',field);
+    },
+
+    beforeShow: function(component, field) {
+      return;
+    },
+
+    beforeDestroy: function(component) {
+      return;
+    }
+  }
+
+  return SampleInsertPlugin;
+}));
+    
